266f73d55669627bfb3ed5f4a83c245f5a939a36
vegxer <vegxer@mail.ru>
2021-04-15 23:53:13 +0300

    set the fields in main.py private

--- main.py
+++ main.py
@@ -14,75 +14,75 @@ from PIL import ImageTk, Image
 
 class FaceDetection:
     def __init__(self):
-        self.image = NONE
-        self.scale_factor = self.min_neighbors = self.min_window_width = self.min_window_height = \
-            self.max_window_width = self.max_window_height = self.pooling_layers = \
-            self.opencv_train_file = self.dlib_train_file = self.image_name = None
-        self.face_found = False
+        self.__image = NONE
+        self.__scale_factor = self.__min_neighbors = self.__min_window_width = self.__min_window_height = \
+            self.__max_window_width = self.__max_window_height = self.__pooling_layers = \
+            self.__opencv_train_file = self.__dlib_train_file = self.__image_name = None
+        self.__face_found = False
 
     def open(self):
         self.__init_root()
         self.__init_menu()
         self.__init_picture_box()
-        self.root.mainloop()
+        self.__root.mainloop()
 
     def __opencv_detection(self):
-        opencv_detector = OpenCVFaceDetection(self.image_name, self.opencv_train_file, self.scale_factor,
-                                              self.min_neighbors, self.min_window_width, self.min_window_height,
-                                              self.max_window_width, self.max_window_height)
-        self.img = opencv_detector.detect_face()
-        if type(self.img) is numpy.ndarray:
+        opencv_detector = OpenCVFaceDetection(self.__image_name, self.__opencv_train_file, self.__scale_factor,
+                                              self.__min_neighbors, self.__min_window_width, self.__min_window_height,
+                                              self.__max_window_width, self.__max_window_height)
+        self.__img = opencv_detector.detect_face()
+        if type(self.__img) is numpy.ndarray:
             self.__show_image()
 
     def __dlib_detection(self):
-        dlib_detector = DlibFaceDetection(self.image_name, self.dlib_train_file, self.pooling_layers)
-        self.img = dlib_detector.detect_face()
-        if type(self.img) is numpy.ndarray:
-            self.img = cv2.cvtColor(self.img, cv2.COLOR_RGB2BGR)
+        dlib_detector = DlibFaceDetection(self.__image_name, self.__dlib_train_file, self.__pooling_layers)
+        self.__img = dlib_detector.detect_face()
+        if type(self.__img) is numpy.ndarray:
+            self.__img = cv2.cvtColor(self.__img, cv2.COLOR_RGB2BGR)
             self.__show_image()
 
     def __show_image(self):
-        if self.img.size != 0:
-            self.face_found = True
-        self.image = ImageTk.PhotoImage(Image.fromarray(cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)))
-        self.picture_box.configure(image=self.image)
-        self.picture_box.image = self.image
+        if self.__img.size != 0:
+            self.__face_found = True
+        self.__image = ImageTk.PhotoImage(Image.fromarray(cv2.cvtColor(self.__img, cv2.COLOR_BGR2RGB)))
+        self.__picture_box.configure(image=self.__image)
+        self.__picture_box.image = self.__image
 
     def __display_source_image(self):
-        self.image_name = filedialog.askopenfilename(filetypes=[('.jpg files', '*.jpg'), ('.png files', '*.png')])
-        if self.image_name:
-            self.image = Image.open(self.image_name)
+        self.__image_name = filedialog.askopenfilename(filetypes=[('.jpg files', '*.jpg'), ('.png files', '*.png')])
+        if self.__image_name:
+            self.__image = Image.open(self.__image_name)
             self.__change_window()
-            self.image = ImageTk.PhotoImage(self.image)
-            self.picture_box.configure(image=self.image)
-            self.picture_box.image = self.image
+            self.__image = ImageTk.PhotoImage(self.__image)
+            self.__picture_box.configure(image=self.__image)
+            self.__picture_box.image = self.__image
 
     def __change_window(self):
-        img_width, img_height = self.image.size
-        self.picture_box.config(width=img_width)
-        self.picture_box.config(height=img_height)
-        if img_width > self.root.winfo_screenwidth() - 40:
-            img_width = self.root.winfo_screenwidth() - 40
-        if img_height > self.root.winfo_screenheight() - 120:
-            img_height = self.root.winfo_screenheight() - 120
-        r = (self.root.winfo_screenwidth() - 20 - (img_width + 25)) / 2
-        t = (self.root.winfo_screenheight() - 90 - (img_height + 25)) / 2
-        self.root.wm_geometry("+%d+%d" % (r, t))
-        self.root.geometry('{}x{}'.format(img_width + 25, img_height + 25))
+        img_width, img_height = self.__image.size
+        self.__picture_box.config(width=img_width)
+        self.__picture_box.config(height=img_height)
+        if img_width > self.__root.winfo_screenwidth() - 40:
+            img_width = self.__root.winfo_screenwidth() - 40
+        if img_height > self.__root.winfo_screenheight() - 120:
+            img_height = self.__root.winfo_screenheight() - 120
+        r = (self.__root.winfo_screenwidth() - 20 - (img_width + 25)) / 2
+        t = (self.__root.winfo_screenheight() - 90 - (img_height + 25)) / 2
+        self.__root.wm_geometry("+%d+%d" % (r, t))
+        self.__root.geometry('{}x{}'.format(img_width + 25, img_height + 25))
 
     def __save_image_as(self):
         if self.__image_correct():
             file_name = filedialog.asksaveasfilename()
             if file_name:
-                cv2.imwrite(file_name + Path(self.image_name).suffix, self.img)
+                cv2.imwrite(file_name + Path(self.__image_name).suffix, self.__img)
 
     def __save_image(self):
         if self.__image_correct():
-            cv2.imwrite(self.image_name, self.img)
+            cv2.imwrite(self.__image_name, self.__img)
 
     def __image_correct(self):
-        if self.image != NONE:
-            if self.face_found:
+        if self.__image != NONE:
+            if self.__face_found:
                 return True
             else:
                 messagebox.showwarning("Save error", "Image hasn't been changed")
@@ -91,78 +91,78 @@ class FaceDetection:
         return False
 
     def __opencv_settings(self):
-        settings_window = SettingsWindowOpenCV(self.opencv_train_file, self.scale_factor, self.min_neighbors
-                                               , self.min_window_width, self.min_window_height,
-                                               self.max_window_width, self.max_window_height)
-        settings_window.open_dialog(self.root)
-        self.opencv_train_file = settings_window.xml_file
-        self.scale_factor = settings_window.scale_factor
-        self.min_neighbors = settings_window.min_neighbors
-        self.min_window_height = settings_window.height_min
-        self.min_window_width = settings_window.width_min
-        self.max_window_height = settings_window.height_max
-        self.max_window_width = settings_window.width_max
+        settings_window = SettingsWindowOpenCV(self.__opencv_train_file, self.__scale_factor, self.__min_neighbors
+                                               , self.__min_window_width, self.__min_window_height,
+                                               self.__max_window_width, self.__max_window_height)
+        settings_window.open_dialog(self.__root)
+        self.__opencv_train_file = settings_window.xml_file
+        self.__scale_factor = settings_window.scale_factor
+        self.__min_neighbors = settings_window.min_neighbors
+        self.__min_window_height = settings_window.height_min
+        self.__min_window_width = settings_window.width_min
+        self.__max_window_height = settings_window.height_max
+        self.__max_window_width = settings_window.width_max
 
     def __dlib_settings(self):
-        settings_window = SettingsWindowDlib(self.dlib_train_file, self.pooling_layers)
-        settings_window.open_dialog(self.root)
-        self.pooling_layers = settings_window.pooling_layers
-        self.dlib_train_file = settings_window.training_file
+        settings_window = SettingsWindowDlib(self.__dlib_train_file, self.__pooling_layers)
+        settings_window.open_dialog(self.__root)
+        self.__pooling_layers = settings_window.pooling_layers
+        self.__dlib_train_file = settings_window.training_file
 
     def __init_root(self):
-        self.root = Tk()
-        self.root.geometry("500x500")
-        self.root.title("Face detection")
-        r = (self.root.winfo_screenwidth() - self.root.winfo_reqwidth()) / 2.5
-        t = (self.root.winfo_screenheight() - self.root.winfo_reqheight()) / 2.5
-        self.root.wm_geometry("+%d+%d" % (r, t))
-        self.root.resizable(width=False, height=False)
-        self.root.iconphoto(True, ImageTk.PhotoImage(Image.open("app_icon.png")))
+        self.__root = Tk()
+        self.__root.geometry("500x500")
+        self.__root.title("Face detection")
+        r = (self.__root.winfo_screenwidth() - self.__root.winfo_reqwidth()) / 2.5
+        t = (self.__root.winfo_screenheight() - self.__root.winfo_reqheight()) / 2.5
+        self.__root.wm_geometry("+%d+%d" % (r, t))
+        self.__root.resizable(width=False, height=False)
+        self.__root.iconphoto(True, ImageTk.PhotoImage(Image.open("app_icon.png")))
 
     def __init_menu(self):
-        self.menubar = Menu(self.root)
+        self.__menubar = Menu(self.__root)
 
-        self.file_menu = Menu(self.menubar, tearoff=0)
-        self.file_menu.add_command(label="Open", command=self.__display_source_image)
-        self.file_menu.add_command(label="Save", command=self.__save_image)
-        self.file_menu.add_command(label="Save as", command=self.__save_image_as)
-        self.menubar.add_cascade(label="File", menu=self.file_menu)
+        self.__file_menu = Menu(self.__menubar, tearoff=0)
+        self.__file_menu.add_command(label="Open", command=self.__display_source_image)
+        self.__file_menu.add_command(label="Save", command=self.__save_image)
+        self.__file_menu.add_command(label="Save as", command=self.__save_image_as)
+        self.__menubar.add_cascade(label="File", menu=self.__file_menu)
 
-        self.menubar.add_separator()
+        self.__menubar.add_separator()
 
-        self.setting_menu = Menu(self.menubar, tearoff=0)
-        self.setting_menu.add_command(label="Haar cascade setting", command=self.__opencv_settings)
-        self.setting_menu.add_command(label="CNN settings", command=self.__dlib_settings)
-        self.menubar.add_cascade(label="Settings", menu=self.setting_menu)
+        self.__setting_menu = Menu(self.__menubar, tearoff=0)
+        self.__setting_menu.add_command(label="Haar cascade setting", command=self.__opencv_settings)
+        self.__setting_menu.add_command(label="CNN settings", command=self.__dlib_settings)
+        self.__menubar.add_cascade(label="Settings", menu=self.__setting_menu)
 
-        self.menubar.add_separator()
+        self.__menubar.add_separator()
 
-        self.detect_menu = Menu(self.menubar, tearoff=0)
-        self.detect_menu.add_command(label="Haar cascade detection", command=self.__opencv_detection)
-        self.detect_menu.add_command(label="CNN detection", command=self.__dlib_detection)
-        self.menubar.add_cascade(label="Detect", menu=self.detect_menu)
+        self.__detect_menu = Menu(self.__menubar, tearoff=0)
+        self.__detect_menu.add_command(label="Haar cascade detection", command=self.__opencv_detection)
+        self.__detect_menu.add_command(label="CNN detection", command=self.__dlib_detection)
+        self.__menubar.add_cascade(label="Detect", menu=self.__detect_menu)
 
-        self.root.config(menu=self.menubar)
+        self.__root.config(menu=self.__menubar)
 
     def __init_picture_box(self):
-        self.scrollbarY = ttk.Scrollbar(self.root, orient=VERTICAL)
-        self.scrollbarY.pack(side=RIGHT, fill=Y)
-        self.scrollbarX = ttk.Scrollbar(self.root, orient=HORIZONTAL)
-        self.scrollbarX.pack(side=BOTTOM, fill=X)
-        self.my_canvas = Canvas(self.root)
-        self.my_canvas.pack(side=TOP, fill=BOTH, expand=1)
-        self.scrollbarX.config(command=self.my_canvas.xview)
-        self.scrollbarY.config(command=self.my_canvas.yview)
+        self.__scrollbarY = ttk.Scrollbar(self.__root, orient=VERTICAL)
+        self.__scrollbarY.pack(side=RIGHT, fill=Y)
+        self.__scrollbarX = ttk.Scrollbar(self.__root, orient=HORIZONTAL)
+        self.__scrollbarX.pack(side=BOTTOM, fill=X)
+        self.__my_canvas = Canvas(self.__root)
+        self.__my_canvas.pack(side=TOP, fill=BOTH, expand=1)
+        self.__scrollbarX.config(command=self.__my_canvas.xview)
+        self.__scrollbarY.config(command=self.__my_canvas.yview)
 
-        self.my_canvas.configure(yscrollcommand=self.scrollbarY.set, xscrollcommand=self.scrollbarX.set)
-        self.my_canvas.bind('<Configure>', lambda e: self.my_canvas.configure(scrollregion=self.my_canvas.bbox("all")))
+        self.__my_canvas.configure(yscrollcommand=self.__scrollbarY.set, xscrollcommand=self.__scrollbarX.set)
+        self.__my_canvas.bind('<Configure>', lambda e: self.__my_canvas.configure(scrollregion=self.__my_canvas.bbox("all")))
 
-        self.frame = Frame(self.my_canvas)
-        self.my_canvas.create_window((0, 0), window=self.frame, anchor="nw")
+        self.__frame = Frame(self.__my_canvas)
+        self.__my_canvas.create_window((0, 0), window=self.__frame, anchor="nw")
 
-        self.picture_box = Label(self.frame, text="Set the settings and upload an image", font="Arial 16", width=39,
-                                 height=15)
-        self.picture_box.pack()
+        self.__picture_box = Label(self.__frame, text="Set the settings and upload an image", font="Arial 16", width=39,
+                                   height=15)
+        self.__picture_box.pack()
 
 
 if __name__ == "__main__":
